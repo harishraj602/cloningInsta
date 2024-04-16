aa81589fede5ef01deacb85d4a393a38574fec34
harish <harishcharlie602@gmail.com>
2024-04-16 15:26:07 +0530

    first commit

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -3,17 +3,9 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 import { getStorage } from "firebase/storage";
+import { firebaseConfig } from "./firebaseconfig";
 
 
-const firebaseConfig = {
-  apiKey:"AIzaSyCzXGhO2zEAIlBBH40E-D_dP6x1IWbm2ks",
-  authDomain:"insta-clone-6976e.firebaseapp.com" ,
-  projectId:"insta-clone-6976e",
-  storageBucket:"insta-clone-6976e.appspot.com",
-  messagingSenderId:"201318624165",
-  appId:"1:201318624165:web:9e5f8c3ac31ef92cd5564c",
-  measurementId:"G-ZV4TB73F46"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
